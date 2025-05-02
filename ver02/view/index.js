95dfda036ebce900840c5fa79d0bd57f05698d5c
jihyeon <mac@EggBook.local>
2025-05-02 11:37:28 +0900

    투두 ui 작성

--- ver02/view/index.js
+++ ver02/view/index.js
@@ -12,9 +12,22 @@ const categoryForm = document.getElementById(FORM.CATEGORY);
 const todoList = document.getElementById(TODO_LIST_ID);
 const categoryList = document.getElementById(CATEGORY_LIST_ID);
 
+const TODO_ELEMENT = (todo) => `
+  <li class="todo-item" id="${todo.id}">
+    <input type="checkbox" class="todo-checkbox"  ${
+      todo.active ? "checked" : ""
+    }>
+    <span class="todo-text">${todo.todo}</span>
+    <button class="todo-delete">Delete</button>
+  </li>`;
+
 export default {
   todoForm,
   categoryForm,
   todoList,
   categoryList,
+
+  ELEMENT: {
+    TODO: TODO_ELEMENT,
+  },
 };
